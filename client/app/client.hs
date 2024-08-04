e5efe514dacd74bd64f9e0a4b6424b360fcaf514
carovas <yulia_schroth@yahoo.fr>
2024-08-04 07:46:18 +0200

    basic client stuff

--- client/app/client.hs
+++ client/app/client.hs
@@ -1,4 +1,35 @@
 module Main where
 
+import System.IO
+import Control.Concurrent
+
+{-
+asks for an ip adress (the port will always be 6969)
+connect to the server
+
+two threads:
+- receiving thread get messages from the server
+- sending thread read input from user and sends it
+-}
+
+{-
+sending thread
+get message from user
+send it
+call the function again
+-}
+sendLoop :: Chan () -> IO ()
+sendLoop endFlags = do
+  x <- getLine
+  if x == "exit"
+  then writeChan endFlags ()
+  else do
+    putStrLn x
+    sendLoop endFlags
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+  endFlags <- newChan
+  forkIO $ sendLoop endFlags
+
+  --mapM_ (\_ -> readChan endFlags) [1..1]
